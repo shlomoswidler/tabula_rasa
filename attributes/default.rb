75ff3ff2c6890579b6ca5c27ffc88b9dedef57d0
Shlomo Swidler <shlomo.swidler@gmail.com>
2014-09-22 18:25:45 +0300

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,4 @@
 default[:tabula_rasa][:chef_version] = '11.10'
 default[:tabula_rasa][:cookbook_url] = 'https://github.com/opscode-cookbooks/mysql.git'
 default[:tabula_rasa][:recipes] = {}
+default[:tabula_rasa][:home_dir] = '/home/tabula-rasa'
