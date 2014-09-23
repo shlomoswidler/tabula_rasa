8ffa21e11cd369ddf4f4c2cc227ed6b11d845a4f
Shlomo Swidler <shlomo.swidler@gmail.com>
2014-09-23 17:02:46 +0300

    Update install_chef_client.rb

--- recipes/install_chef_client.rb
+++ recipes/install_chef_client.rb
@@ -4,6 +4,15 @@
 sha256checksum = nil
 
 package_dir = node[:tabula_rasa][:home_dir] + '/cache'
+
+directory package_dir do
+  recursive true
+  action :create
+  user 'root'
+  group 'root'
+  mode 00700
+end
+
 remote_file package_dir + '/chef-client-package.deb' do
   checksum sha256checksum if sha256checksum
   source 'https://www.getchef.com/chef/download?p=ubuntu&pv=12.04&v=11.10&m=x86_64'
