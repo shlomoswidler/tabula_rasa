526ec22cc4cf02a0aa1da1c837066a69a01ebd62
Shlomo Swidler <shlomo.swidler@gmail.com>
2014-09-24 05:05:28 +0300

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,8 +2,7 @@ default[:tabula_rasa][:chef_version] = '11.10'
 default[:tabula_rasa][:recipes] = {}
 default[:tabula_rasa][:home_dir] = '/home/tabula-rasa'
 
-default[:tabula_rasa][:scm] = {}
-default[:tabula_rasa][:scm][:type] = "none" 
+default[:tabula_rasa][:scm][:type] = 'none'
 default[:tabula_rasa][:scm][:repository] = null
 default[:tabula_rasa][:scm][:user] = null
 default[:tabula_rasa][:scm][:password] = null
