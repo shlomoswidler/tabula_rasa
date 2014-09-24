44e14c9c8d0ff3b00db0cbc6410f3ccfeae3b7ea
Shlomo Swidler <shlomo.swidler@gmail.com>
2014-09-24 05:13:25 +0300

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,10 +3,10 @@ default[:tabula_rasa][:recipes] = {}
 default[:tabula_rasa][:home_dir] = '/home/tabula-rasa'
 
 default[:tabula_rasa][:scm][:type] = 'none'
-default[:tabula_rasa][:scm][:repository] = null
-default[:tabula_rasa][:scm][:user] = null
-default[:tabula_rasa][:scm][:password] = null
-default[:tabula_rasa][:scm][:revision] = null
-default[:tabula_rasa][:scm][:ssh_key] = null
+default[:tabula_rasa][:scm][:repository] = nil
+default[:tabula_rasa][:scm][:user] = nil
+default[:tabula_rasa][:scm][:password] = nil
+default[:tabula_rasa][:scm][:revision] = nil
+default[:tabula_rasa][:scm][:ssh_key] = nil
 default[:tabula_rasa][:scm][:enable_submodules] = true
 
