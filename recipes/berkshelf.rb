c51825de821034f5b47639f9efe69fc640e0b0a7
Shlomo Swidler <shlomo.swidler@gmail.com>
2014-09-28 15:39:23 +0300

    Update berkshelf.rb

--- recipes/berkshelf.rb
+++ recipes/berkshelf.rb
@@ -1,4 +1,12 @@
 # Based on opsworks-cookbooks/opsworks_berkshelf/providers/runner.rb
+berks_install_options = if node['opsworks_berkshelf']['version'].to_i >= 3
+  "vendor #{berkshelf_cookbooks_dir}"
+else
+  "install --path #{berkshelf_cookbooks_dir}"
+end
+
+berks_install_options += ' --debug' if node['opsworks_berkshelf']['debug']
+berks_install_command = "/opt/aws/opsworks/local/bin/berks #{berks_install_options}"
 
 site_cookbooks_dir = ::File.join(node[:tabula_rasa][:home_dir], 'site-cookbooks')
 berkshelf_cookbooks_dir = ::File.join(node[:tabula_rasa][:home_dir],'berkshelf-cookbooks')
@@ -14,6 +22,20 @@ end
 
 ruby_block 'Install the cookbooks specified in the Tabula Rasa\'s cookbook Berksfile and their dependencies' do
   block do
+    
+    # TODO: This snippet is repeated in the only_if block below. Bad style.
+     berksfile_top = ::File.join(site_cookbooks_dir, 'Berksfile')
+    # only return Berksfile if there is exactly one folder no matter if this folder contains a berksfile or not
+    folders = Dir.glob(::File.join(site_cookbooks_dir, '*')).select { |f| ::File.directory? f }
+
+    if ::File.exist? berksfile_top
+      berksfile = berksfile_top
+    elsif folders.size == 1
+      berksfile = ::File.join(folders.first, 'Berksfile')
+    else
+      berksfile = ''
+    end
+    
     Chef::Log.info OpsWorks::ShellOut.shellout(
       berks_install_command,
       :cwd => ::File.dirname(berksfile),
@@ -24,32 +46,19 @@ ruby_block 'Install the cookbooks specified in the Tabula Rasa\'s cookbook Berks
   end
 
   only_if do
+    # TODO: This snippet is a duplicate of that foudn in the block above. Bad style.
+    berksfile_top = ::File.join(site_cookbooks_dir, 'Berksfile')
+    # only return Berksfile if there is exactly one folder no matter if this folder contains a berksfile or not
+    folders = Dir.glob(::File.join(site_cookbooks_dir, '*')).select { |f| ::File.directory? f }
+
+    if ::File.exist? berksfile_top
+      berksfile = berksfile_top
+    elsif folders.size == 1
+      berksfile = ::File.join(folders.first, 'Berksfile')
+    else
+      berksfile = ''
+    end  
     ::File.exist?('/opt/aws/opsworks/local/bin/berks') && ::File.exist?(berksfile)
   end
 end
 
-def berksfile
-  berksfile_top = ::File.join(site_cookbooks_dir, 'Berksfile')
-  # only return Berksfile if there is exactly one folder no matter if this folder contains a berksfile or not
-  folders = Dir.glob(::File.join(site_cookbooks_dir, '*')).select { |f| ::File.directory? f }
-
-  if ::File.exist? berksfile_top
-    berksfile_top
-  elsif folders.size == 1
-    ::File.join(folders.first, 'Berksfile')
-  else
-    ''
-  end
-end
-
-def berks_install_command
-  options = if node['opsworks_berkshelf']['version'].to_i >= 3
-    "vendor #{berkshelf_cookbooks_dir}"
-  else
-    "install --path #{berkshelf_cookbooks_dir}"
-  end
-
-  options += ' --debug' if node['opsworks_berkshelf']['debug']
-
-  "/opt/aws/opsworks/local/bin/berks #{options}"
-end
