3b507e24eff360df6fbc1794862f68ba17c2e9a0
Shlomo Swidler <shlomo.swidler@gmail.com>
2014-09-30 23:33:22 +0300

    Create update_tabula_rasa_cookbooks.rb

---
+++ recipes/update_tabula_rasa_cookbooks.rb
@@ -0,0 +1,12 @@
+site_cookbooks_dir = ::File.join(node[:tabula_rasa][:home_dir], 'site-cookbooks')
+
+directory site_cookbooks_dir do
+  action :delete
+  recursive true
+
+  only_if do
+    ::File.directory?(site_cookbooks_dir)
+  end
+end
+
+include_recipe "tabula_rasa::prapare_cookbooks"
