a39d0456d33fa4224598bcc489bb40bea8aa38fe
Shlomo Swidler <shlomo.swidler@gmail.com>
2014-09-30 23:07:01 +0300

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name             'tabula_rasa'
 maintainer       'Shlomo Swidler'
 maintainer_email 'shlomo.swidler@orchestratus.com'
 license          'Copyright 2014, Shlomo Swidler. All Rights Reserved'
-description      'Experiment'
+description      'Run community cookbooks from within AWS OpsWorks without clashing with the older versions in opsworks-cookbooks'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
