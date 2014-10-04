715d3bfde0b8217371b3664d91d4038322301436
Shlomo Swidler <shlomo.swidler@gmail.com>
2014-10-04 23:14:48 +0300

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'shlomo.swidler@orchestratus.com'
 license          'Apache 2.0'
 description      'Run community cookbooks from within AWS OpsWorks without clashing with the older versions in opsworks-cookbooks'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.2.0'
 
 supports "ubuntu", "= 12.04"
 
