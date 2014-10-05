d69d2f4b107a0f4ca876537569a636a90d126262
Shlomo Swidler <shlomo.swidler@gmail.com>
2014-10-05 11:53:45 +0300

    Isolate berks cache dir

--- recipes/berkshelf.rb
+++ recipes/berkshelf.rb
@@ -1,6 +1,7 @@
 # Based on opsworks-cookbooks/opsworks_berkshelf/providers/runner.rb
 site_cookbooks_dir = ::File.join(node[:tabula_rasa][:home_dir], 'site-cookbooks')
 berkshelf_cookbooks_dir = ::File.join(node[:tabula_rasa][:home_dir],'berkshelf-cookbooks')
+berks_cache_dir = ::File.join(node[:tabula_rasa][:home_dir], 'cache', 'berks')
 
 berks_install_options = if node['opsworks_berkshelf'] && node['opsworks_berkshelf']['version'].to_i >= 3
   "vendor #{berkshelf_cookbooks_dir}"
@@ -20,11 +21,20 @@ directory berkshelf_cookbooks_dir do
   end
 end
 
+directory berks_cache_dir do
+  recursive true
+  action :create
+  
+  only_if do
+    !::File.directory?(berks_cache_dir)
+  end
+end
+
 ruby_block 'Install the cookbooks specified in the Tabula Rasa cookbook\'s Berksfile and their dependencies' do
   block do
     
     # TODO: This snippet is repeated in the only_if block below. Bad style.
-     berksfile_top = ::File.join(site_cookbooks_dir, 'Berksfile')
+    berksfile_top = ::File.join(site_cookbooks_dir, 'Berksfile')
     # only return Berksfile if there is exactly one folder no matter if this folder contains a berksfile or not
     folders = Dir.glob(::File.join(site_cookbooks_dir, '*')).select { |f| ::File.directory? f }
 
@@ -40,7 +50,7 @@ ruby_block 'Install the cookbooks specified in the Tabula Rasa cookbook\'s Berks
       berks_install_command,
       :cwd => ::File.dirname(berksfile),
       :environment  => {
-        "BERKSHELF_PATH" => ::File.join(node[:tabula_rasa][:home_dir], 'cache'),
+        "BERKSHELF_PATH" => berks_cache_dir,
         "LC_ALL" => "en_US.UTF-8",
         "LANG" => "en_US.UTF-8"
       }
